35908036f19a6120496dfd9f696c72c10c720342
xyzuan <xyzuannihboss@gmail.com>
2024-03-19 15:01:45 +0700

    main: Update build.sh & compose

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-docker build -f Dockerfile --label mf-ecommerce-host -t xyzuan/mf-ecommerce-host:1.0 .
-docker build -f Dockerfile --label mf-list-product -t xyzuan/mf-list-product:1.0 .
+docker build -f ./mf-ecommerce-host/Dockerfile --label mf-ecommerce-host -t xyzuan/mf-ecommerce-host:1.2 .
+docker build -f ./mf-list-product/Dockerfile --label mf-list-product -t xyzuan/mf-list-product:1.2 .
+docker build -f ./mf-payment-and-cart/Dockerfile --label mf-payment-and-cart -t xyzuan/mf-payment-and-cart:1.2 .
+
+docker push xyzuan/mf-ecommerce-host:1.2
+docker push xyzuan/mf-list-product:1.2
+docker push xyzuan/mf-payment-and-cart:1.2
