3df4710f09cc90ec8dc40aafed022a6ca51f0e7c
xyzuan <xyzuannihboss@gmail.com>
2024-03-19 17:53:06 +0700

    main: Update changes from mf remote

---
+++ README.md
@@ -0,0 +1,34 @@
+# eCommerce Module Federation
+
+## Feature
+
+- Microfrontend Product List
+- Microfrontend Detail Product
+- Microfrontend Payment & Cart
+
+## Live Preview
+
+Visit this site [ecommerce.xyzuan.my.id](https://ecommerce.xyzuan.my.id/)
+
+## Preview
+
+![Home](https://github.com/INTERN-FE/len-ecommerce/blob/main/public/home.png?raw=true)
+
+## Installation
+
+### Prerequisites
+
+- Docker
+- Nginx
+- Portainer ( Optional )
+
+### Installation Steps
+
+1. Clone the repository and submodules
+2. Change desired env variable within `./build.sh & ./docker-compose.yml`
+3. Build the mf app to docker image locally: `./build.sh`
+4. Served in desired URL
+
+## License
+
+This project is licensed under the LEN Intern MSIB6 - see the [LICENSE.md](LICENSE.md) file for details.
--- build.sh
+++ build.sh
@@ -1,15 +1,26 @@
 ## Replace with desired docker hub username
 cd mf-ecommerce-host
-docker build -f Dockerfile --label mf-ecommerce-host -t xyzuan/mf-ecommerce-host:1.2 .
+docker build -f Dockerfile \
+--build-arg BASE_HOST_URL="https://ecommerce.xyzuan.my.id/" \
+--build-arg MF_LIST_PRODUCT_HOST_URL="https://mf-list-product.xyzuan.my.id/" \
+--build-arg MF_PAYMENT_CART_HOST_URL="https://mf-cart-payment.xyzuan.my.id/" \
+--label mf-ecommerce-host \
+-t xyzuan/mf-ecommerce-host:2.2 .
 cd ../
 
 cd mf-list-product
-docker build -f Dockerfile --label mf-list-product -t xyzuan/mf-list-product:1.2 .
+docker build -f Dockerfile \
+--build-arg BASE_HOST_URL="https://mf-list-product.xyzuan.my.id/" \
+--label mf-list-product \
+-t xyzuan/mf-list-product:2.2 .
 cd ../
 
 cd mf-payment-and-cart
-docker build -f Dockerfile --label mf-payment-and-cart -t xyzuan/mf-payment-and-cart:1.2 .
+docker build -f Dockerfile\
+--build-arg BASE_HOST_URL="https://mf-cart-payment.xyzuan.my.id/" \
+--label mf-payment-and-cart \
+-t xyzuan/mf-payment-and-cart:2.2 .
 
-docker push xyzuan/mf-ecommerce-host:1.2
-docker push xyzuan/mf-list-product:1.2
-docker push xyzuan/mf-payment-and-cart:1.2
+docker push xyzuan/mf-ecommerce-host:2.2
+docker push xyzuan/mf-list-product:2.2
+docker push xyzuan/mf-payment-and-cart:2.2
