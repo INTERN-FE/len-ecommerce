d67b08d32a26799043dee30a3a6b613fbacbc44c
xyzuan <xyzuannihboss@gmail.com>
2024-03-19 15:03:26 +0700

    main: [TMP] Fix up build directory

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
-docker build -f ./mf-ecommerce-host/Dockerfile --label mf-ecommerce-host -t xyzuan/mf-ecommerce-host:1.2 .
-docker build -f ./mf-list-product/Dockerfile --label mf-list-product -t xyzuan/mf-list-product:1.2 .
-docker build -f ./mf-payment-and-cart/Dockerfile --label mf-payment-and-cart -t xyzuan/mf-payment-and-cart:1.2 .
+## Replace with desired docker hub username
+cd mf-ecommerce-host
+docker build -f Dockerfile --label mf-ecommerce-host -t xyzuan/mf-ecommerce-host:1.2 .
+cd ../
+
+cd mf-list-product
+docker build -f Dockerfile --label mf-list-product -t xyzuan/mf-list-product:1.2 .
+cd ../
+
+cd mf-payment-and-cart
+docker build -f Dockerfile --label mf-payment-and-cart -t xyzuan/mf-payment-and-cart:1.2 .
 
 docker push xyzuan/mf-ecommerce-host:1.2
 docker push xyzuan/mf-list-product:1.2
